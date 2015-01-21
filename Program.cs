1f6e838197d922c8177d8dbf15640eecfcea7569
Jeff Nygren <JNygren.Programmer@GMail.com>
2015-01-21 07:49:07 -0600

    Initial Commit.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿// Preprocessor symbols must be defined before the first token in the file.
+#define AUDIBLE
+//#define PROMPTFORCLOSE
+
+// Namespaces
+using System;
+using System.Windows;   // For Clipboard class. (Add reference to PresentationCore.)
+
+namespace PutItOnCB
+{
+    class Program
+    {
+        private static string cbText = "Tlhfckoctbcr$1955#170&Sex";
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PutItOnCB Ver. 1.1 - Friday, June 28, 2013 - Jeff Nygren");
+
+            try
+            {
+                Clipboard.SetText(CBText);  // Put text on ClipBoard
+            }
+            catch (Exception ex)
+            {
+                Console.Write("\n{0}  Press any key to continue...", ex.Message); Console.ReadKey();
+            }
+
+#if AUDIBLE
+            System.Media.SystemSounds.Beep.Play();  // Indicate that program ran.
+#endif
+
+#if PROMPTFORCLOSE
+            Console.Write("\nPress any key to exit..."); Console.ReadKey();
+#endif
+        }
+
+        private static string CBText {
+            get { return cbText; }
+            set { cbText = value; }
+        }
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,16 @@
+	PutItOnCB - ReadMe
+
+"Put In On the ClipBoard" puts a fixed string on the system clipboard.
+
+Program.cs is all of the code for this program. If you want to build the 
+application, create a new C# .NET 3.5 'Console' app in Visual Studio. (This was
+created using VS2010.) Replace the VS-generated Program.cs file with this one.
+That's all there is to it. 
+
+Note: No installation or uninstall is required. Just run the program on any
+Windows system with the appropriate version of .NET.
+
+
+-- Jeff Nygren
+
+================================================================================
