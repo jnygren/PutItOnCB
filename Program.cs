d070898068ed311072626c2bb5c0f815b83073cc
Jeff Nygren <JNygren.Programmer@GMail.com>
2015-06-16 07:44:05 -0500

    Ver. 1.2 - Added workAround for CLIPBRD_E_CANT_OPEN exception. Updated AssemblyInfo.

--- Program.cs
+++ Program.cs
@@ -11,19 +11,26 @@ namespace PutItOnCB
     class Program
     {
         private static string cbText = "Tlhfckoctbcr$1955#170&Sex";
+        private static string errMessage = "";
 
         [STAThread]
         static void Main(string[] args)
         {
-            Console.WriteLine("PutItOnCB Ver. 1.1 - Friday, June 28, 2013 - Jeff Nygren");
+            bool success = false;
+            Console.WriteLine("PutItOnCB Ver. 1.2 - Wednesday, January 21, 2015 - Jeff Nygren");
 
-            try
+            // WorkAround for CLIPBRD_E_CANT_OPEN exception
+            for (int i = 0; i < 10; i++)
             {
-                Clipboard.SetText(CBText);  // Put text on ClipBoard
+                if (success = TrySetText(CBText))
+                    break;
+                System.Threading.Thread.Sleep(300);
             }
-            catch (Exception ex)
+
+            if (!success)
             {
-                Console.Write("\n{0}  Press any key to continue...", ex.Message); Console.ReadKey();
+                // Display exception message, wait for keypress.
+                Console.Write("\n{0}  Press any key to continue...", errMessage); Console.ReadKey();
             }
 
 #if AUDIBLE
@@ -35,6 +42,32 @@ namespace PutItOnCB
 #endif
         }
 
+
+        /// <summary>
+        /// Clipboard.SetText with exception handling
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns>success or failure</returns>
+        private static bool TrySetText(string text)
+        {
+            bool result = true;
+
+            try
+            {
+                Clipboard.SetText(CBText);  // Put text on ClipBoard
+            }
+            catch (Exception ex)
+            {
+                // Save exception message.
+                errMessage = ex.Message;
+                result = false;
+            }
+
+            return result;
+        }
+
+
+        // Properties
         private static string CBText {
             get { return cbText; }
             set { cbText = value; }
